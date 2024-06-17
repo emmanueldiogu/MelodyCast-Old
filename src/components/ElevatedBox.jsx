7d1f554b52b8cb482024f48fd7662ca08706645d
Lolis007 <ladan.aisha@yahoo.com>
2024-06-17 13:20:59 -0400

    Updates to elevated box

--- src/components/ElevatedBox.jsx
+++ src/components/ElevatedBox.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const ElevatedBox = ({ children, className }) => {
+const ElevatedBox = ({ children, className, customStyle }) => {
   return (
     <div
       className={`flex text-white rounded-2xl shadow-glass backdrop-blur-md bg-black/40 ${className}`}
+      style={customStyle}
     >
       {children}
     </div>
@@ -14,6 +15,7 @@ const ElevatedBox = ({ children, className }) => {
 ElevatedBox.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  customStyle: PropTypes.object,
 };
 
 export default ElevatedBox;
