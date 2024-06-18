81f6f552e51d4d43602426e86b09a8e0c5ec3eb7
Lolis007 <ladan.aisha@yahoo.com>
2024-06-18 16:06:17 -0400

    Elevated boxes imported into account section

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,12 +1,194 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import ElevatedBox from "../components/ElevatedBox";
+import backgroundImage from "./../assets/bg-image.jpg";
 
-const Account = props => {
-    return (
-        <div>Account</div>
-    )
-}
+const Account = (props) => {
+  return (
+    <>
+      <div className="md:col-span-8 flex flex-col gap-[22.5px] 2xl:gap-8">
+        <div className="text-clamp-20 font-medium text-white max-md:max-w-full h-fit">
+          for Cloudy Days
+        </div>
+        <div className="grid grid-cols-4 gap-4 2xl:gap-6 overflow-y-auto">
+          <div className="flex flex-col">
+            <div className="flex flex-col grow justify-center px-2.5 pt-2.5 pb-3 w-full text-xs text-white rounded-lg bg-zinc-900/40 max-md:mt-6">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/23a612ac9be9414662d74031f65785f2bd5d5cdad28bb9099e0b7d9aa4c9b2d2?apiKey=fdc3c41cbf0743359cb4456bd858c80a&"
+                className="self-center aspect-square w-full"
+              />
+              <div className="flex flex-col mt-3">
+                <a href="#" className="font-bold text-ellipsis">
+                  Gloom Grooves
+                </a>
+                <div
+                  className="mt-1 font-medium line-clamp-2"
+                  title="Mellow tunes for gray skies. Mellow tunes for gray skies"
+                >
+                  Mellow tunes for gray skies. Mellow tunes for gray skies.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <div className="flex flex-col grow justify-center px-2.5 pt-2.5 pb-3 w-full text-xs text-white rounded-lg bg-zinc-900/40 max-md:mt-6">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/23a612ac9be9414662d74031f65785f2bd5d5cdad28bb9099e0b7d9aa4c9b2d2?apiKey=fdc3c41cbf0743359cb4456bd858c80a&"
+                className="self-center aspect-square w-full"
+              />
+              <div className="flex flex-col mt-3">
+                <a href="#" className="font-bold text-ellipsis">
+                  Gloom Grooves
+                </a>
+                <div
+                  className="mt-1 font-medium line-clamp-2"
+                  title="Mellow tunes for gray skies. Mellow tunes for gray skies"
+                >
+                  Mellow tunes for gray skies. Mellow tunes for gray skies.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <div className="flex flex-col grow justify-center px-2.5 pt-2.5 pb-3 w-full text-xs text-white rounded-lg bg-zinc-900/40 max-md:mt-6">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/23a612ac9be9414662d74031f65785f2bd5d5cdad28bb9099e0b7d9aa4c9b2d2?apiKey=fdc3c41cbf0743359cb4456bd858c80a&"
+                className="self-center aspect-square w-full"
+              />
+              <div className="flex flex-col mt-3">
+                <a href="#" className="font-bold text-ellipsis">
+                  Gloom Grooves
+                </a>
+                <div
+                  className="mt-1 font-medium line-clamp-2"
+                  title="Mellow tunes for gray skies. Mellow tunes for gray skies"
+                >
+                  Mellow tunes for gray skies. Mellow tunes for gray skies.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <div className="flex flex-col grow justify-center px-2.5 pt-2.5 pb-3 w-full text-xs text-white rounded-lg bg-zinc-900/40 max-md:mt-6">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/23a612ac9be9414662d74031f65785f2bd5d5cdad28bb9099e0b7d9aa4c9b2d2?apiKey=fdc3c41cbf0743359cb4456bd858c80a&"
+                className="self-center aspect-square w-full"
+              />
+              <div className="flex flex-col mt-3">
+                <a href="#" className="font-bold text-ellipsis">
+                  Gloom Grooves
+                </a>
+                <div
+                  className="mt-1 font-medium line-clamp-2"
+                  title="Mellow tunes for gray skies. Mellow tunes for gray skies"
+                >
+                  Mellow tunes for gray skies. Mellow tunes for gray skies.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <div className="flex flex-col grow justify-center px-2.5 pt-2.5 pb-3 w-full text-xs text-white rounded-lg bg-zinc-900/40 max-md:mt-6">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/23a612ac9be9414662d74031f65785f2bd5d5cdad28bb9099e0b7d9aa4c9b2d2?apiKey=fdc3c41cbf0743359cb4456bd858c80a&"
+                className="self-center aspect-square w-full"
+              />
+              <div className="flex flex-col mt-3">
+                <a href="#" className="font-bold text-ellipsis">
+                  Gloom Grooves
+                </a>
+                <div
+                  className="mt-1 font-medium line-clamp-2"
+                  title="Mellow tunes for gray skies. Mellow tunes for gray skies"
+                >
+                  Mellow tunes for gray skies. Mellow tunes for gray skies.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <div className="flex flex-col grow justify-center px-2.5 pt-2.5 pb-3 w-full text-xs text-white rounded-lg bg-zinc-900/40 max-md:mt-6">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/23a612ac9be9414662d74031f65785f2bd5d5cdad28bb9099e0b7d9aa4c9b2d2?apiKey=fdc3c41cbf0743359cb4456bd858c80a&"
+                className="self-center aspect-square w-full"
+              />
+              <div className="flex flex-col mt-3">
+                <a href="#" className="font-bold text-ellipsis">
+                  Gloom Grooves
+                </a>
+                <div
+                  className="mt-1 font-medium line-clamp-2"
+                  title="Mellow tunes for gray skies. Mellow tunes for gray skies"
+                >
+                  Mellow tunes for gray skies. Mellow tunes for gray skies.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <div className="flex flex-col grow justify-center px-2.5 pt-2.5 pb-3 w-full text-xs text-white rounded-lg bg-zinc-900/40 max-md:mt-6">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/23a612ac9be9414662d74031f65785f2bd5d5cdad28bb9099e0b7d9aa4c9b2d2?apiKey=fdc3c41cbf0743359cb4456bd858c80a&"
+                className="self-center aspect-square w-full"
+              />
+              <div className="flex flex-col mt-3">
+                <a href="#" className="font-bold text-ellipsis">
+                  Gloom Grooves
+                </a>
+                <div
+                  className="mt-1 font-medium line-clamp-2"
+                  title="Mellow tunes for gray skies. Mellow tunes for gray skies"
+                >
+                  Mellow tunes for gray skies. Mellow tunes for gray skies.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <div className="flex flex-col grow justify-center px-2.5 pt-2.5 pb-3 w-full text-xs text-white rounded-lg bg-zinc-900/40 max-md:mt-6">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/23a612ac9be9414662d74031f65785f2bd5d5cdad28bb9099e0b7d9aa4c9b2d2?apiKey=fdc3c41cbf0743359cb4456bd858c80a&"
+                className="self-center aspect-square w-full"
+              />
+              <div className="flex flex-col mt-3">
+                <a href="#" className="font-bold text-ellipsis">
+                  Gloom Grooves
+                </a>
+                <div
+                  className="mt-1 font-medium line-clamp-2"
+                  title="Mellow tunes for gray skies. Mellow tunes for gray skies"
+                >
+                  Mellow tunes for gray skies. Mellow tunes for gray skies.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <ElevatedBox className="md:col-span-4 py-4  px-6 flex flex-col justify-between text-2xl font-medium text-white">
+        Report Incorrect Weather
+        <div className="flex gap-2.5 mt-1">
+          <div className="flex-1 font-light text-sm">
+            If you notice any inaccuracies in the weather information provided
+            by our app, please report them using the form below:
+          </div>
+        </div>
+        <div className="flex gap-2.5 mt-1">
+            <div className="font-semibold text-s">Location</div>
+          </div>
+      </ElevatedBox>
+    </>
+  );
+};
 
-Account.propTypes = {}
+Account.propTypes = {};
 
-export default Account
+export default Account;
