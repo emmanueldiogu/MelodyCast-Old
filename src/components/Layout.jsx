8d6678bce22d86c14567772c342d703e538de864
vandinh52 <157068699+vandinh52@users.noreply.github.com>
2024-06-19 19:51:25 -0400

    update layout setting to use image fetch api

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -12,7 +12,12 @@ import ProfileDetails from "./ProfileDetails";
 
 const Layout = ({ children }) => {
   const currentPath = useLocation().pathname;
-  const { isLoading } = useSearch();
+  const { isLoading, weatherImage } = useSearch();
+
+  const selectRandomImage = () => {
+    let index = parseInt(Math.random() * weatherImage.length);
+    return weatherImage[index].src?.landscape;
+  }
 
   return (
     <>
@@ -20,14 +25,14 @@ const Layout = ({ children }) => {
       <div
         className="h-screen overflow-hidden relative"
         style={{
-          background: `linear-gradient(0deg, rgba(0, 0, 0, 0.20) 0%, rgba(0, 0, 0, 0.20) 100%), url(${backgroundImage}) lightgray 50% / cover no-repeat`,
+          background: `linear-gradient(0deg, rgba(0, 0, 0, 0.20) 0%, rgba(0, 0, 0, 0.20) 100%), url(${selectRandomImage() || backgroundImage}) lightgray 50% / cover no-repeat`,
         }}
       >
         {/* <div className="absolute top-0 right-0 bottom-0 left-0 bg-black/20"></div> */}
         <div className="container mx-auto">
           <div className="relative grid grid-cols-1 gap-6 xxl:gap-8 py-3 xxl:py-5 grid-rows-[auto_auto_1fr]">
             <TopBar />
-            {(currentPath === "/account") ? <ProfileDetails/>: <WeatherDetail />}
+            {(currentPath === "/account") ? <ProfileDetails /> : <WeatherDetail />}
             <Content>
               {currentPath !== "/account" && <Menu />}
               {children}
